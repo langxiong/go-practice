5b1ff419379c348d2a282c93be4f7b8df1d32966
xiongl <x583194811l@gmail.com>
2017-10-05 18:39:42 +0800

    nil: throw exception

--- main.go
+++ main.go
@@ -4,13 +4,52 @@ import (
 	"fmt"
 )
 
-func main() {
-	power := 1000
+type Student struct {
+	Name string
+	Power int
+	Classmate *Student
+}
 
-	_, power = "xiongl", getPower()
-	fmt.Printf("xiongl's power over %d", power)
+func main() {
+	xiongl := Student {
+		"xiongl",
+		10000,
+		nil,
+	}
+
+	xiongl1 := Student {
+
+	}
+
+	xiongl2 := Student {
+		Name: "xiongl",
+	}
+
+	xiongl.Super()
+	xiongl1.Super()
+	xiongl2.Super()
+
+	fmt.Printf("%s's power is %d\n", xiongl.Name, xiongl.Power)
+	fmt.Printf("%s's power is %d\n", xiongl1.Name, xiongl1.Power)
+	fmt.Printf("%s's power is %d\n", xiongl2.Name, xiongl2.Power)
+
+	xl := NewStudent("xl", 99999)
+	xl.Super()
+	fmt.Printf("%s's power is %d, classmate %s\n", xl.Name, xl.Power, xl.Classmate.Name)
 }
 
 func getPower() int {
 	return 9001
+}
+
+func (s *Student) Super() {
+	s.Power += 10000
+}
+
+func NewStudent(name string, power int) *Student {
+	return &Student {
+		Name: name,
+		Power: power,
+		Classmate: nil,
+	}
 }
