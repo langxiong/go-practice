96ce0571fd382623e289dc1592669e74403c4c3d
xiongl <x583194811l@gmail.com>
2017-10-06 18:47:41 +0800

    slices: backup removeAtIndex

--- main.go
+++ main.go
@@ -22,10 +22,9 @@ func (s *Saiyan) Introduce() {
 }
 
 func main() {
-	scores := make([]int, 5)
-
-	scores = append(scores, 3)
-
+	scores := []int{1, 2, 3, 4, 5}
+	slice := scores[2:4]
+	slice[0] = 999
 	fmt.Println(scores)
 
 	names := []string{
@@ -64,8 +63,23 @@ func main() {
 		powers := extractPowerPoint(&saiyans)
 		fmt.Println(powers)
 	}
+	{
+		scores := []int{1, 2, 3, 4, 5}
+		scores = scores[:len(scores)-1]
+		fmt.Println(scores)
+	}
+	{
+		scores := []int{1, 2, 3, 4, 5}
+		scores = removeAtIndex(scores, 2)
+		fmt.Println(scores)
+	}
 }
 
+func removeAtIndex(source []int, index int) []int {
+	lastIndex := len(source) - 1
+	source[index], source[lastIndex] = source[lastIndex], source[index]
+	return source[:lastIndex]
+}
 func extractPowerPoint(saiyans *[]Saiyan) []int {
 	powers := make([]int, len(*saiyans))
 
