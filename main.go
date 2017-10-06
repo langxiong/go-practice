ddbf336ecfe2777fb0fe80c273d6b1c8d7fa68b9
xiongl <x583194811l@gmail.com>
2017-10-06 18:52:44 +0800

    slice: copy

--- main.go
+++ main.go
@@ -2,100 +2,20 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sort"
 )
 
-type Person struct {
-	Name string
-}
-
-func (p *Person) Introduce() {
-	fmt.Printf("Hi, I'm %s\n", p.Name)
-}
-
-type Saiyan struct {
-	*Person
-	Power int
-}
-
-func (s *Saiyan) Introduce() {
-	fmt.Printf("Hi, this is %s\n", s.Name)
-}
-
 func main() {
-	scores := []int{1, 2, 3, 4, 5}
-	slice := scores[2:4]
-	slice[0] = 999
-	fmt.Println(scores)
+	scores := make([]int, 100)
 
-	names := []string{
-		"xiongl",
-		"fangy",
+	for i := 0; i < 100; i++ {
+		scores[i] = int(rand.Int31n(1000))
 	}
 
-	fmt.Println(names)
+	sort.Ints(scores)
 
-	xl := Saiyan {
-		&Person {"xiongl",},
-		9999,
-	}
-
-	fy := Saiyan {
-		&Person {"fangyi",},
-		10000,
-	}
-
-	{
-		saiyans := []*Saiyan{};
-		
-		saiyans = append(saiyans, &xl)
-		saiyans = append(saiyans, &fy)
-
-		powers := extractPower(saiyans)
-		fmt.Println(powers)
-	}
-
-	{
-		saiyans := []Saiyan{};
-		
-		saiyans = append(saiyans, xl)
-		saiyans = append(saiyans, fy)
-
-		powers := extractPowerPoint(&saiyans)
-		fmt.Println(powers)
-	}
-	{
-		scores := []int{1, 2, 3, 4, 5}
-		scores = scores[:len(scores)-1]
-		fmt.Println(scores)
-	}
-	{
-		scores := []int{1, 2, 3, 4, 5}
-		scores = removeAtIndex(scores, 2)
-		fmt.Println(scores)
-	}
-}
-
-func removeAtIndex(source []int, index int) []int {
-	lastIndex := len(source) - 1
-	source[index], source[lastIndex] = source[lastIndex], source[index]
-	return source[:lastIndex]
-}
-func extractPowerPoint(saiyans *[]Saiyan) []int {
-	powers := make([]int, len(*saiyans))
-
-	for i, s := range(*saiyans) {
-		powers[i] = s.Power
-	}
-
-	return powers
-}
-
-func extractPower(saiyans []*Saiyan) []int{
-	powers := make([]int, len(saiyans))
-
-	for i, s := range(saiyans) {
-		powers[i] = s.Power
-	}
-
-	return powers
+	worst := make([]int, 5)
+	copy(worst[2:5], scores[:5])
+	fmt.Println(worst)
 }
