184ff02f7fba104a1209b3c493568ea98804e65f
xiongl <x583194811l@gmail.com>
2017-10-07 13:56:41 +0800

    map: as member

--- main.go
+++ main.go
@@ -19,16 +19,37 @@ func main() {
 	copy(worst[2:5], scores[:5])
 	fmt.Println(worst)
 
-	lookup := make(map[string]int)
+	lookup := make(map[string]int, 1000)
 
 	lookup["xiongl"] = 9001
 
 	power, exists := lookup["fangyi"]
 
 	fmt.Println(power, exists)
-	fmt.Println(lookup)
+	fmt.Println(lookup, " ", len(lookup))
 	lookup["fangyi"] = 0
-	fmt.Println(lookup)	
+	fmt.Println(lookup, " ", len(lookup))
 	delete(lookup, "fangyi")
-	fmt.Println(lookup)	
+	fmt.Println(lookup, " ", len(lookup))
+
+	{
+		xiongl := &Saiyan {
+			Name: "xiongl",
+			Friends: make(map[string]*Saiyan),
+		}
+
+		xiongl.Friends["fangyi"] = &Saiyan {
+			Name: "fangyi",
+			Friends: make(map[string]*Saiyan),
+		}
+
+		fmt.Println(xiongl)
+	}
 }
+
+type Saiyan struct {
+	Name string
+	Friends map[string]*Saiyan
+}
+
+
