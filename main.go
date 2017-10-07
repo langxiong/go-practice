78c81912149b82c7e156c1a1e9fd4fb5f7c01e71
xiongl <x583194811l@gmail.com>
2017-10-07 13:49:34 +0800

    map: delete done

--- main.go
+++ main.go
@@ -18,4 +18,17 @@ func main() {
 	worst := make([]int, 5)
 	copy(worst[2:5], scores[:5])
 	fmt.Println(worst)
+
+	lookup := make(map[string]int)
+
+	lookup["xiongl"] = 9001
+
+	power, exists := lookup["fangyi"]
+
+	fmt.Println(power, exists)
+	fmt.Println(lookup)
+	lookup["fangyi"] = 0
+	fmt.Println(lookup)	
+	delete(lookup, "fangyi")
+	fmt.Println(lookup)	
 }
